a03d5bc2fdd3e4541b8033d90ffaf0cd3cf5ac15
Johan Alexander Londoño Bedoya <johannlondonob@gmail.com>
2020-02-20 20:54:54 -0500

    Primer commit Algoritmos parte 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,62 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Primer algoritmo:
+        /*1. Se requiere un algoritmo para obtener la edad promedio de un grupo de N alumnos. Si algún alumno tiene más de 18 años,
+        se muestra el promedio que se lleva sin contar el alumno de 18 años. EL usuario decide si desea cerrar el programa o vuelve a ejecutarlo.*/
+        Scanner scan = new Scanner(System.in);
+        boolean go = true;
+        int edadAlumno = 0;
+        int edadesAlumnos = 0;
+        int contadorAlumno = 0;
+        double edadPromedio = 0;
+
+        while(go) {
+            System.out.print("¿Edad del alumno? ");
+            edadAlumno = scan.nextInt();
+            edadesAlumnos += edadAlumno;
+            contadorAlumno++;
+
+            if (edadAlumno > 18) {
+                edadesAlumnos -= edadAlumno;
+                contadorAlumno -= 1;
+                edadPromedio = edadesAlumnos / contadorAlumno;
+                System.out.println("Promedio de edad hasta ahora: " + edadPromedio);
+            } else {
+                edadPromedio = edadesAlumnos / contadorAlumno;
+            }
+
+            if (contadorAlumno >= 2) {
+                System.out.print("¿Desea continuar con el ingreso de edad por alumno? \n Presione 1, para continuar \n Presione 2, para detener y calcular promedio\n Su respuesta: ");
+                int res = scan.nextInt();
+                go = res == 1;
+            }
+        }
+        System.out.println("La edad promedio de " + contadorAlumno + " alumnos, es " + edadPromedio);
+
+        // Segundo algoritmo:
+        /* Se requiere un algoritmo para obtener la suma de diez cantidades mediante la utilización de un ciclo for */
+        ArrayList<Integer> numeros = new ArrayList<>();
+        for (int i = 0; i <= 9; i++) {
+            numeros.add((int)Math.floor(Math.random()*(5-20)+20));
+        }
+        String num = "";
+        int suma = 0;
+        for (int i = 0; i < numeros.size(); i++) {
+            suma += numeros.get(i);
+            num += numeros.get(i) + ", ";
+        }
+        System.out.println("Números usados para la suma: " + num + "y estos sumaron: " + suma);
+
+        // Tercer algoritmo:
+        /* Se requiere un algoritmo para obtener la estatura promedio de un grupo de personas, cuyo número
+        de miembros se desconoce, el ciclo debe efectuarse siempre y cuando se tenga una estatura registrada. */
+
+
+    }
+}
